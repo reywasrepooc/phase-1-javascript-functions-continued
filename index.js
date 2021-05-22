f9e85c53d04d5bd891ec62444b4468d6bfb443f0
Cooper Sawyer <cooper.sawyer@powerhrg.com>
2021-05-22 12:19:22 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Your code here
+function saturdayFun(activity) {
+    if (saturdayFun = activity) {
+        return `This Saturday, I want to ${activity}!`
+    } else {
+        return `This Saturday, I want to roller-skate!`
+    }
+}
+
+function mondayWork(activity) {
+    if (mondayWork = activity) {
+        return `This Monday, I will ${activity}.`
+    } else {
+        return `This Monday, I will go to the office.`
+    }
+}
+
+function wrapAdjective(flair="*") {
+    return function(arg="special") {
+        return `You are ${flair}${arg}${flair}!`
+    }
+}
